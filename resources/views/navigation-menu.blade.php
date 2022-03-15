4338a597336ef5fe0285bd6854ff0bf9419eab2e
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2022-03-15 16:17:18 -0400

    cambio en el menu en el orden de las opciones

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,43 +13,45 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Entrega
                     </x-jet-nav-link>
                     @role('admin')
-                    <x-jet-nav-link href="{{ route('page.user') }}" :active="request()->routeIs('page.user')">
-                        Usuario
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.category') }}" :active="request()->routeIs('page.category')">
-                        Categoria
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.subcategory') }}" :active="request()->routeIs('page.subcategory')">
-                        Subcategoria
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.product') }}" :active="request()->routeIs('page.product')">
-                        Producto
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.way') }}" :active="request()->routeIs('page.way')">
-                        Contacto
-                    </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.control') }}" :active="request()->routeIs('page.control')">
+                            Control
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.user') }}" :active="request()->routeIs('page.user')">
+                            Usuario
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.category') }}" :active="request()->routeIs('page.category')">
+                            Categoria
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.subcategory') }}"
+                            :active="request()->routeIs('page.subcategory')">
+                            Subcategoria
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.product') }}" :active="request()->routeIs('page.product')">
+                            Producto
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.way') }}" :active="request()->routeIs('page.way')">
+                            Contacto
+                        </x-jet-nav-link>
                     @endrole
                     @role('admin|user')
-                    <x-jet-nav-link href="{{ route('page.client') }}" :active="request()->routeIs('page.client')">
-                        Cliente
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.order') }}" :active="request()->routeIs('page.order')">
-                        Pedido
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.control') }}" :active="request()->routeIs('page.control')">
-                        Control
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('page.calendar') }}" :active="request()->routeIs('page.calendar')">
-                        Calendario
-                    </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.client') }}" :active="request()->routeIs('page.client')">
+                            Cliente
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.order') }}" :active="request()->routeIs('page.order')">
+                            Pedido
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.calendar') }}"
+                            :active="request()->routeIs('page.calendar')">
+                            Calendario
+                        </x-jet-nav-link>
                     @endrole
                     @role('admin|report')
-                    <x-jet-nav-link href="{{ route('page.report') }}" :active="request()->routeIs('page.report')">
-                        Reporte
-                    </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('page.report') }}" :active="request()->routeIs('page.report')">
+                            Reporte
+                        </x-jet-nav-link>
                     @endrole
                 </div>
             </div>
@@ -61,11 +63,15 @@
                         <x-jet-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
                                         {{ Auth::user()->currentTeam->name }}
 
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                     </button>
                                 </span>
@@ -79,7 +85,8 @@
                                     </div>
 
                                     <!-- Team Settings -->
-                                    <x-jet-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                    <x-jet-dropdown-link
+                                        href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
                                         {{ __('Team Settings') }}
                                     </x-jet-dropdown-link>
 
@@ -110,16 +117,23 @@
                     <x-jet-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                <button
+                                    class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                    <img class="h-8 w-8 rounded-full object-cover"
+                                        src="{{ Auth::user()->profile_photo_url }}"
+                                        alt="{{ Auth::user()->name }}" />
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                                         {{ Auth::user()->name }}
 
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                     </button>
                                 </span>
@@ -148,8 +162,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                         onclick="event.preventDefault();
+                                <x-jet-dropdown-link href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                     {{ __('Log Out') }}
                                 </x-jet-dropdown-link>
@@ -161,10 +174,14 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -175,43 +192,48 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                Entrega
             </x-jet-responsive-nav-link>
             @role('admin')
-            <x-jet-responsive-nav-link href="{{ route('page.user') }}" :active="request()->routeIs('page.user')">
-                Usuario
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.category') }}" :active="request()->routeIs('page.category')">
-                Categoria
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.subcategory') }}" :active="request()->routeIs('page.subcategory')">
-                Subcategoria
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.product') }}" :active="request()->routeIs('page.product')">
-                Producto
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.way') }}" :active="request()->routeIs('page.way')">
-                Contacto
-            </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.control') }}"
+                    :active="request()->routeIs('page.control')">
+                    Control
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.user') }}" :active="request()->routeIs('page.user')">
+                    Usuario
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.category') }}"
+                    :active="request()->routeIs('page.category')">
+                    Categoria
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.subcategory') }}"
+                    :active="request()->routeIs('page.subcategory')">
+                    Subcategoria
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.product') }}"
+                    :active="request()->routeIs('page.product')">
+                    Producto
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.way') }}" :active="request()->routeIs('page.way')">
+                    Contacto
+                </x-jet-responsive-nav-link>
             @endrole
             @role('admin|user')
-            <x-jet-responsive-nav-link href="{{ route('page.client') }}" :active="request()->routeIs('page.client')">
-                Cliente
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.order') }}" :active="request()->routeIs('page.order')">
-                Pedido
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.control') }}" :active="request()->routeIs('page.control')">
-                Control
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('page.calendar') }}" :active="request()->routeIs('page.calendar')">
-                Calendario
-            </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.client') }}" :active="request()->routeIs('page.client')">
+                    Cliente
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.order') }}" :active="request()->routeIs('page.order')">
+                    Pedido
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.calendar') }}"
+                    :active="request()->routeIs('page.calendar')">
+                    Calendario
+                </x-jet-responsive-nav-link>
             @endrole
             @role('admin|report')
-            <x-jet-responsive-nav-link href="{{ route('page.report') }}" :active="request()->routeIs('page.report')">
-                Reporte
-            </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('page.report') }}" :active="request()->routeIs('page.report')">
+                    Reporte
+                </x-jet-responsive-nav-link>
             @endrole
         </div>
 
@@ -220,7 +242,8 @@
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 mr-3">
-                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                            alt="{{ Auth::user()->name }}" />
                     </div>
                 @endif
 
@@ -232,12 +255,14 @@
 
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
-                <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                <x-jet-responsive-nav-link href="{{ route('profile.show') }}"
+                    :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
                 </x-jet-responsive-nav-link>
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                    <x-jet-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
+                    <x-jet-responsive-nav-link href="{{ route('api-tokens.index') }}"
+                        :active="request()->routeIs('api-tokens.index')">
                         {{ __('API Tokens') }}
                     </x-jet-responsive-nav-link>
                 @endif
@@ -246,8 +271,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-jet-responsive-nav-link href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                    <x-jet-responsive-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
                                     this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-jet-responsive-nav-link>
@@ -262,12 +286,14 @@
                     </div>
 
                     <!-- Team Settings -->
-                    <x-jet-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+                    <x-jet-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
+                        :active="request()->routeIs('teams.show')">
                         {{ __('Team Settings') }}
                     </x-jet-responsive-nav-link>
 
                     @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                        <x-jet-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
+                        <x-jet-responsive-nav-link href="{{ route('teams.create') }}"
+                            :active="request()->routeIs('teams.create')">
                             {{ __('Create New Team') }}
                         </x-jet-responsive-nav-link>
                     @endcan
--- resources/views/pages/control.blade.php
+++ resources/views/pages/control.blade.php
@@ -8,16 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @livewire('component-control')
+                @livewire('component-management')
             </div>
         </div>
     </div>
-    @push('scripts')
-        <script type="text/javascript">
-            function clickToCopyText(contentToCopy) {
-                var textContent = document.getElementById(contentToCopy).innerText;
-                navigator.clipboard.writeText(textContent);
-            }
-        </script>
-    @endpush
 </x-app-layout>
