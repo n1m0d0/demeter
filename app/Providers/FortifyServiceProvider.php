009fa904269d18473b01c0a9372cb9c6451d25aa
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2022-03-15 10:03:03 -0400

    cambio en la validacion del login para la verificacion de que el usuario existe

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,7 +41,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::authenticateUsing(function (Request $request) {
             $user = User::where('email', $request->email)->first();
 
-            if ($user->status == User::ACTIVO) {
+            if ($user && ($user->status == User::ACTIVO)) {
                 if ($user && Hash::check($request->password, $user->password)) {
                     return $user;
                 }
